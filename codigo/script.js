cb97b08c4abb731707614fdbc4506b503223c8f8
valmirbezerra19 <valmirbezerra70@gmail.com>
2021-11-16 20:12:30 -0300

    alteração

--- codigo/script.js
+++ codigo/script.js
@@ -3,13 +3,13 @@ const transactionsUl = document.querySelector('#transactions')
 const dummyTransactions = [
     {id: 1, name: 'Bolo de Brigadeiro', amount: -20},
     {id: 2, name: 'Salário', amount: 300},
-    {id: 3, name: 'Torta de frango', amount: -10}
+    {id: 3, name: 'Torta de frango', amount: -10},
     {id: 4, name: 'Violão', amount: -150}
 ]
 
 const addTransactionIntoDOM = transaction =>{
     const operator = transaction.amount <0 ? '-' : '+'
-    const CSSCclass = transaction.amount <0 ? 'minus' : 'plus'
+    const CSSClass = transaction.amount <0 ? 'minus' : 'plus'
     const amountWithoutOperator = Math.abs(transaction.amount)
     const li = document.createElement('li')
     
